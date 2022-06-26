f76f9588cca0a35d1edeff07ac7ef6e5717f0e69
Dat Van <vanthanhphuongdat1309@gmail.com>
2022-06-26 22:28:41 +0700

    create home page & movies page

--- src/App.js
+++ src/App.js
@@ -1,35 +1,35 @@
 import React from "react";
-import "swiper/scss";
+import { Route, Routes } from "react-router-dom";
 import "swiper/css/navigation";
-import Banner from "./components/banner/Banner";
-import MovieList from "./components/movie/MovieList";
+import "swiper/scss";
+
+import Main from "./components/layout/Main";
+import HomePage from "./pages/HomePage";
+import MoviesPage from "./pages/MoviesPage";
 
 function App() {
   return (
     <>
-      <header className="header flex items-center justify-center gap-x-5 text-white py-10 mb-5">
-        <span className="text-primary">Home</span>
-        <span>Movie</span>
-      </header>
-      <Banner />
-      <section className="movies-layout page-container pb-20">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Now playing
-        </h2>
-        <MovieList type="now_playing" />
-      </section>
-      <section className="movies-layout page-container pb-20">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Top rated
-        </h2>
-        <MovieList type="top_rated" />
-      </section>
-      <section className="movies-layout page-container pb-20">
-        <h2 className="capitalize text-white mb-10 text-3xl font-bold">
-          Trending
-        </h2>
-        <MovieList type="popular" />
-      </section>
+      <Routes>
+        <Route element={<Main />}>
+          <Route
+            path="/"
+            element={
+              <>
+                <HomePage />
+              </>
+            }
+          />
+          <Route
+            path="/movies"
+            element={
+              <>
+                <MoviesPage />
+              </>
+            }
+          />
+        </Route>
+      </Routes>
     </>
   );
 }
---
+++ src/pages/MoviesPage.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+const MoviesPage = () => {
+  return <div>Lorem</div>;
+};
+
+export default MoviesPage;
