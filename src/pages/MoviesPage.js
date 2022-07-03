af5c7bf581abcbb658345598c38a70cafd260393
Dat Van <vanthanhphuongdat1309@gmail.com>
2022-07-04 01:08:08 +0700

    add pagination

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from "react";
+import ReactPaginate from "react-paginate";
 import useSWR from "swr";
 
 import { fetcher, API_KEY } from "../config";
@@ -6,11 +7,17 @@ import MovieCard from "../components/movie/MovieCard";
 import useDebounce from "../hooks/useDebounce";
 import Loading from "../components/loading/Loading";
 
+const itemsPerPage = 20;
+
 const MoviesPage = () => {
+  const [pageCount, setPageCount] = useState(0);
+  const [itemOffset, setItemOffset] = useState(0);
+  const [nextPage, setNextPage] = useState(1);
+
   const [filter, setFilter] = useState("");
   const filterDebounce = useDebounce(filter, 500);
   const [url, setUrl] = useState(
-    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`
+    `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${nextPage}`
   );
   const handleFilterChange = (e) => {
     setFilter(e.target.value);
@@ -21,16 +28,29 @@ const MoviesPage = () => {
   useEffect(() => {
     if (filterDebounce) {
       setUrl(
-        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${filterDebounce}`
+        `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&query=${filterDebounce}&page=${nextPage}`
       );
     } else {
-      setUrl(`https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}`);
+      setUrl(
+        `https://api.themoviedb.org/3/movie/popular?api_key=${API_KEY}&page=${nextPage}`
+      );
     }
-  }, [filterDebounce]);
+  }, [filterDebounce, nextPage]);
 
   const isLoading = !data && !error;
 
   const movies = data?.results || [];
+
+  useEffect(() => {
+    if (!data || !data.total_results) return;
+    setPageCount(Math.ceil(data.total_results / itemsPerPage));
+  }, [data, itemOffset]);
+
+  const handlePageClick = (e) => {
+    const newOffset = (e.selected * itemsPerPage) % data.total_results;
+    setItemOffset(newOffset);
+    setNextPage(e.selected + 1);
+  };
 
   return (
     <div className="py-10 page-container">
@@ -43,6 +63,7 @@ const MoviesPage = () => {
             onChange={handleFilterChange}
           />
         </div>
+
         <button className="p-4 bg-primary text-white">
           <svg
             xmlns="http://www.w3.org/2000/svg"
@@ -60,11 +81,26 @@ const MoviesPage = () => {
           </svg>
         </button>
       </div>
+
       {isLoading && <Loading />}
+
       <div className="grid grid-cols-4 gap-10">
         {!isLoading &&
           movies.length > 0 &&
           movies.map((item) => <MovieCard key={item.id} item={item} />)}
+      </div>
+
+      <div className="mt-10">
+        <ReactPaginate
+          breakLabel="..."
+          nextLabel="next >"
+          onPageChange={handlePageClick}
+          pageRangeDisplayed={5}
+          pageCount={pageCount}
+          previousLabel="< previous"
+          renderOnZeroPageCount={null}
+          className="pagination"
+        />
       </div>
     </div>
   );
