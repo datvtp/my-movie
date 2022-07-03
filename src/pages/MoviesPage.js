b7be1bf92b26f34a2e773a0a869cdedc035cebba
Dat Van <vanthanhphuongdat1309@gmail.com>
2022-07-04 01:18:28 +0700

    change search input style

--- src/pages/MoviesPage.js
+++ src/pages/MoviesPage.js
@@ -54,32 +54,29 @@ const MoviesPage = () => {
 
   return (
     <div className="py-10 page-container">
-      <div className="flex mb-10">
+      <div className="flex items-center bg-slate-800 rounded-full mb-10">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-10 w-10 mx-4"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          strokeWidth="2"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+          />
+        </svg>
         <div className="flex-1">
           <input
             type="text"
-            className="w-full p-4 bg-slate-800 outline-none"
-            placeholder="Type here to search..."
+            className="w-full p-4 bg-inherit outline-none"
+            placeholder="Type here to search ..."
             onChange={handleFilterChange}
           />
         </div>
-
-        <button className="p-4 bg-primary text-white">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className="h-6 w-6"
-            fill="none"
-            viewBox="0 0 24 24"
-            stroke="currentColor"
-            strokeWidth="2"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
-            />
-          </svg>
-        </button>
       </div>
 
       {isLoading && <Loading />}
