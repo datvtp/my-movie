c4fd9c9f5a868583234fdcc2e4e9703b09675ce6
Dat Van <vanthanhphuongdat1309@gmail.com>
2022-07-05 20:16:42 +0700

    fix route

--- src/App.js
+++ src/App.js
@@ -22,7 +22,7 @@ function App() {
             }
           />
           <Route
-            path="/movies"
+            path="/movie"
             element={
               <>
                 <MoviesPage />
